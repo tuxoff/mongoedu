48a4a3b9bbc00f05d2ab90b5995706fab424d7b6
tuxoff <tuxoff@gmail.com>
2014-02-12 01:06:02 +0200

    next week starts here

---
+++ test.py
@@ -0,0 +1,58 @@
+# __author__ = 'Owner'
+# things = {"animals": ["dog", "cat", "zebra"]}
+# things['animals'].append('waka-waka')
+# new_things = []
+#
+# for item in things['animals']:
+#    print(item)
+#
+# new_things.append(item)
+# print('last')
+# print(new_things)
+# sum = 0
+# numbers = [1,2,3,5,8]
+# for i in numbers:
+#   sum = sum + i
+#   print(sum)
+#   print(i)
+# print (i)
+
+# people = {'name':'Bob', 'hometown': "Palo Alto", 'favorite_color': 'red'}
+# for item in people:
+#   if (item == 'favorite_color'):
+#      print(people[item])
+
+# people = ['name', 'Bob', 'hometown', "Palo Alto", 'favorite_color', 'red']
+#
+# i = 0
+# while (i<len(people)):
+#     print(people[i])
+#     i = i+1
+
+
+# fruit = ['apple', 'orange', 'grape', 'orange', 'grape', 'grape']
+#
+# def analyze_list(l):
+#     counts = {}
+#     for item in l:
+#         if item in counts:
+#             counts[item] = counts[item] + 1
+#         else:
+#             counts[item] = 1
+#     return counts
+#
+# count = analyze_list(fruit)
+# print (count)
+
+
+
+# import sys
+# try:
+#     print(5/0)
+# except:
+#     print("Exception", sys.exc_info())
+#
+# print('Show must go on')
+
+
+
---
+++ views/form_result.tpl
@@ -0,0 +1,2 @@
+<a href="/">return basic</a>
+{{content}}
